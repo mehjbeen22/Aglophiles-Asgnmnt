70a4427b3c81742a71b1b12d0708c422269382fd
mehjbeen22 <mehjbeen222navgurukul.org>
2024-05-22 20:06:00 +0530

    +1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import MoviesCatalog from './components/MoviesCatalog';
